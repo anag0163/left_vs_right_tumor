e4f71a492e7cfe55f3d5bf4aa5479e13c739be8f
Anjali Agrawal <anjalisugarland@gmail.com>
2023-09-30 21:47:35 -0500

    added code to compare scores between left and right

--- analyze_deleteriousness_scores.py
+++ analyze_deleteriousness_scores.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from scipy.stats import mannwhitneyu
+
 
 df = pd.read_excel('C:\\Users\\anjal\\Downloads\\fm.xlsx', sheet_name='fm')
 df2 = pd.read_excel('C:\\Users\\anjal\\Downloads\\fm.clinicaldata.xlsx', sheet_name='fm.clinicaldata')
@@ -15,10 +17,13 @@ for index, row in df2.iterrows():
 
 for index, row in df.iterrows():
     tumor_sample = row['Tumor_Sample_Barcode']
-    if (tumor_sample in dictionary_storing_left_right):
+    if (tumor_sample in dictionary_storing_left_right and row['SIFT_score'] != '.' and pd.isna(row['SIFT_score']) == False):
         if (dictionary_storing_left_right[tumor_sample] == "left"):
             scores_for_left.append(row['SIFT_score'])
         else:
             scores_for_right.append(row['SIFT_score'])
 print(scores_for_right)
 print(scores_for_left)
+statistic, p_value = mannwhitneyu(scores_for_right, scores_for_left, alternative='less')
+print(f"Wilcoxon statistic: {statistic}")
+print(f"P-value: {p_value}")
